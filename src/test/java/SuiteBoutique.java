5753329e0b151bd1aa8e3ae0673f41cdaed6172e
HAWA SYLLA <hawasylla@Host-002.lan>
2026-03-17 13:28:11 +0100

    ci:add workflow

--- src/test/java/SuiteBoutique.java
+++ src/test/java/SuiteBoutique.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
 
-public class SuiteBoutique {
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        ArticleTest.class,
+        PanierTest.class,
+        PanierReductionTest.class,
+        ServiceCommandeTest.class
+})
+class SuiteBoutique {
 }
