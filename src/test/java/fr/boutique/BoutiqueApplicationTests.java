c65808de8b1339b0ba351f872fdc43442f660672
HAWA SYLLA <hawasylla@Host-002.lan>
2026-03-23 15:59:23 +0100

    fix: add classes to SpringBootTest annotation

--- src/test/java/fr/boutique/BoutiqueApplicationTests.java
+++ src/test/java/fr/boutique/BoutiqueApplicationTests.java
@@ -3,7 +3,7 @@ package fr.boutique;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BoutiqueApplication.class)
 class BoutiqueApplicationTests {
 
     @Test
